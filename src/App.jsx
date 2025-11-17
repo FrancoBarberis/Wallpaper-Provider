279813bcade4579ac05feea53c48f9d3c861fbdc
Franco Barberis <francobarberissic3@gmail.com>
2025-11-16 21:32:57 -0300

    Probando fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [images,setImages]= useState([])
+  const headers = {
+    Authorization: "efKmjO98NTW2P6B0yW0jwk6dgHw8H8gYKfK4zlpteXvQDi8PBHOVmnkL"
+  }
+ fetch("https://api.pexels.com/v1/search?query=nature",{
+  headers
+ })
+ .then(res=>res.json)
+ .then(data=>{
+  console.log(data);
+ })
+ .catch(error=>{
+  console.error("Error: ", error);
+ })
+
   return (
+    
     <div>
       <h1 className='bg-blue-300'>GALERIA 3d</h1>
     </div>
